bfb00c0debc429c048c11415b8e5babed039973b
akaris <katayama.bac4c9ca@gmail.com>
2020-10-23 14:45:34 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: {with: /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/}
+  validates :password, length: { in: 8..32 }, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
+  
+  
+  
+  has_secure_password
+end
